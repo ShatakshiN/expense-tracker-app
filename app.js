6995d130eb4017fd564a764bf02165d102f308a5
ShatakshiN <shatakshinimare27@gmail.com>
2025-04-05 00:04:20 +0530

    nosql branch

--- app.js
+++ app.js
@@ -4,63 +4,17 @@ const fs = require('fs');
 const nativePath = require('path');
 const app  = express();
 const cors = require('cors');
-const sequelize = require('./util/database');
+const  mongoConnect = require('./util/database');
 const bodyParser = require('body-parser');
 
 require('dotenv').config();
 
-//routes
-const loginAndSignUpRoute = require('./route/loginAndSignUp');
-const expenseRoute = require('./route/expense');
-const delRoute = require('./route/del');
-const premiumRoute = require('./route/premium');
-const forgotPasswordRoute = require('./route/forgotPassword');
-
-// Models
-const Users = require('./models/users');
-const Expense = require('./models/expense');
-const Order = require('./models/order');
-const resetPassword  = require('./models/forgot password');
-const FileURL = require('./models/fileURL');
-
-
-
-// append access history in access.log
-/* const accessLogStream = fs.createWriteStream(nativePath.join(__dirname, 'access.log'), { flag: 'a' }); */
-
 app.use(bodyParser.json()); //specifically parse json formatted req bodies
 app.use(cors());
 
-//routes
-app.use(loginAndSignUpRoute);
-app.use(expenseRoute);
-app.use(delRoute);
-app.use(premiumRoute);
-app.use(forgotPasswordRoute);
 
-
-app.use(express.static('views'));
-app.use(express.static('views/public'));
-
-// Associations
-Expense.belongsTo(Users, { constraints: true, onDelete: 'CASCADE' });
-Users.hasMany(Expense);
-
-Order.belongsTo(Users, { constraints: true, onDelete: 'CASCADE' });
-Users.hasMany(Order);
-
-Users.hasMany(resetPassword);
-resetPassword.belongsTo(Users, { constraints: true, onDelete: 'CASCADE' });
-
-Users.hasMany(FileURL);
-FileURL.belongsTo(Users, { constraints: true, onDelete: 'CASCADE' });
-
-sequelize.sync()
-    .then(()=>{
-        app.listen(process.env.PORT || 4000)
-        console.log('server is running on 4000')
-
-    })
-    .catch((error)=>{
-        console.log(error);
+mongoConnect(() => {
+    app.listen(4000, () => {
+        console.log('Server listening on port 4000');
     });
+});
