aa01c5da307fa58bd0f07c09725acef55c3876ad
ShatakshiN <147754096+ShatakshiN@users.noreply.github.com>
2025-07-16 22:18:15 +0530

    Update del.js

--- controllers/del.js
+++ controllers/del.js
@@ -1,31 +1,26 @@
 const express = require('express');
 require('dotenv').config();
-// Middleware
-const middleware = require('../middleware/auth');
-
-// Mongoose Expense model
+// Models
 const Expense = require('../models/expense');
 
 exports.deleteExp = async(req,res,next) =>{
   try {
         const expenseId = req.params.expenseId;
 
-        // Find the expense by ID and userId
         const expenseToDelete = await Expense.findOne({
-            _id: expenseId,
-            userId: req.user.id
+            where: {
+                id: expenseId,
+                SignUpId: req.user.id
+            }
         });
 
         if (!expenseToDelete) {
             return res.status(404).json({ message: "Expense not found" });
         }
 
-        // Delete the expense
-        await Expense.deleteOne({ _id: expenseId });
-
+        await expenseToDelete.destroy();
         return res.status(200).json({ message: "Expense deleted successfully" });
     } catch (error) {
-        return res.status(500).json({ message: "Failed to delete expense", error: error.message });
+        res.status(500).json({ message: "Failed to delete expense", error: error.message });
     }
 };
-
