8c7d277dcb7d4fee4cc873e761c5859fa9b80c6f
ShatakshiN <147754096+ShatakshiN@users.noreply.github.com>
2025-07-16 21:00:59 +0530

    Update forgotPassword.js

--- route/forgotPassword.js
+++ route/forgotPassword.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const forgotPasswordController = require('../controllers/forgotPaasword');
 const auth = require('../middlewares/auth');
 
-router.post('/forgotPassword', auth, )
+router.post('/forgotPassword', auth, forgotPasswordController.  )
 module.exports = router;
