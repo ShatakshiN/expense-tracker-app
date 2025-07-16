854b5fff69adc3752a9f81cb8a77ad53f7c50fa2
ShatakshiN <147754096+ShatakshiN@users.noreply.github.com>
2025-07-16 21:56:30 +0530

    Update premium.js

--- route/premium.js
+++ route/premium.js
@@ -7,5 +7,7 @@ router.get('/buy-premium', auth, premiumController.buyPremium);
 router.post('/updatetransectionstatus', auth, premiumController.updateTransectionStatus);
 router.get('/check-premium-status', auth, premiumController.checkPremium);
 router.get('/premium/LeaderBoard', auth, premiumController.leaderBoard);
+router.get("/download-expense", auth ,premiumController.downloadExp);
+router.get('/downloaded-files', auth,premiumController.downloadedExpFiles);
 
 module.exports = router;
