1fe9ab6767b24721641e133f762119162cc99d9c
ShatakshiN <147754096+ShatakshiN@users.noreply.github.com>
2025-07-16 20:36:33 +0530

    Update del.js

--- route/del.js
+++ route/del.js
@@ -4,7 +4,8 @@ const delController = require('../controllers/del');
 const auth = require('../middleware/auth');
 
 //router.get('/messages/:receiverId', auth, chatController.getChatMessages);
-router.delete('/delete-expense/:expenseId', auth, delController.)
+
+router.delete('/delete-expense/:expenseId', auth, delController.deleteExp);
 
 
 module.exports = router;
