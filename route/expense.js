b69e9355c0fa6de7f25c930e51fec3480bf858a6
ShatakshiN <147754096+ShatakshiN@users.noreply.github.com>
2025-07-16 20:49:41 +0530

    Update expense.js

--- route/expense.js
+++ route/expense.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
-
+const expController = require('../controllers/expense');
 require('dotenv').config();
 
 //middleware
@@ -10,79 +10,10 @@ const middleware = require('../middleware/auth');
 // Models
 const Expense = require('../models/expense');
 
-
-router.post('/daily-expense', middleware, async (req, res) => {
-    try {
-        const { description, amount, date, category } = req.body;
-
-        const expenseData = await Expense.create({
-            date,
-            description,
-            amount,
-            category,
-            userId: req.user.id 
-        });
-
-        res.status(201).json({ expense: expenseData });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
-
-router.get('/daily-expense', middleware, async (req, res) => {
-    try {
-        const expenses = await Expense.find({ userId: req.user.id }); 
-        res.status(200).json({ allUserOnScreen: expenses });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
+router.post('/daily-expense', auth, expController.postDailyExp);
+router.get('/daily-expense', auth, expController.getDailyExp);
+router.get('/monthly-expense', auth, expController.getMonthlyExp);
+router.get('/yearly-expense', auth, expController.getYearlyExp);
 
 
-router.get('/monthly-expense', middleware, async (req, res) => {
-    try {
-        const userId = req.user.id;
-
-        const monthlyExpenses = await Expense.aggregate([
-            { $match: { userId: new mongoose.Types.ObjectId(userId) } },
-            {
-                $group: {
-                    _id: { $dateToString: { format: "%Y-%m", date: "$date" } },
-                    totalExpense: { $sum: "$amount" }
-                }
-            },
-            { $sort: { _id: 1 } }
-        ]);
-
-        res.status(200).json({ monthlyExpenses });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
-
-
-
-router.get('/yearly-expense', middleware, async (req, res) => {
-    try {
-        const userId = req.user.id;
-
-        const yearlyExpenses = await Expense.aggregate([
-            { $match: { userId: new mongoose.Types.ObjectId(userId) } },
-            {
-                $group: {
-                    _id: { $dateToString: { format: "%Y", date: "$date" } },
-                    totalExpense: { $sum: "$amount" }
-                }
-            },
-            { $sort: { _id: 1 } }
-        ]);
-
-        res.status(200).json({ yearlyExpenses });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
-
-
-module.exports = router;
 
